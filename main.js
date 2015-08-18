7265a7914d60e883e7533a5407e08c4d0a35cc28
Vitor Leal <vitorleal1@gmail.com>
2015-08-18 17:51:57 -0300

    add keyboard shortcuts

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
-var app     = require('app'),
-    Browser = require('browser-window'),
-    win     = null;
+var app      = require('app'),
+    Browser  = require('browser-window'),
+    shortcut = require('global-shortcut'),
+    win      = null;
 
 app.on('window-all-closed', function () {
   if (process.platform != 'darwin') {
     app.quit();
   }
+});
+
+app.on('will-quit', function () {
+  shortcut.unregisterAll();
 });
 
 // App on ready
@@ -21,6 +26,16 @@ app.on('ready', function () {
     win = null;
   });
 
+  // Load the game html
   win.loadUrl('file://' + __dirname + '/index.html');
+
+  //Shortcuts
+  shortcut.register('ctrl+r', function () {
+    win.reload();
+  });
+
+  shortcut.register('ctrl+q', function () {
+    win.close();
+  });
 });
 
